29c2c03ffa34be3fe7b2eda2121cedc074167bcf
Delano Senior <dsenior101@hotmail.com>
2021-02-14 23:01:12 -0500

    All exercises completed

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
@@ -23,6 +24,20 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    """Render a person's profile."""
+
+    return render_template('profile.html', name="Delano Senior",date=format_date_joined(date_joined))
+
+
+date_joined = datetime.date(2019,2,7)
+def format_date_joined(date_joined):
+    """Output a person's join date."""
+    date=date_joined.strftime("%B, %Y")
+    return date
+    # return date
 
 
 ###
